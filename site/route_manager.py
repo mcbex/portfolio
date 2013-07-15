038c206d61fd3560f2cdcb807e32c47603384643
mbecks <beckwith.goldberg@gmail.com>
2013-07-15 12:56:09 -0400

    route manager class with methods to fetch info for view functions

---
+++ site/route_manager.py
@@ -0,0 +1,24 @@
+from redisutils import RedisUtils
+
+class RouteManager:
+
+    def __init__(self):
+        self.redis = RedisUtils()
+
+    def get_page_template(self, page_id):
+        page = self.redis.get_page(page_id)
+        if page and page['template']:
+            return page['template']
+        else:
+            return None
+
+    def get_n_pages(self, num_pages):
+        range = (0, num_pages)
+        pages = self.redis.get_pages_byrank(range)
+        page_data = None
+        if pages and len(pages):
+            page_data = {}
+            for p in pages:
+                page_data[p] = self.redis.get_page(p)
+        return page_data
+
