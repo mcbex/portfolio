36c88a6a924caba27c3141cfcd65852acfb55644
mbecks <beckwith.goldberg@gmail.com>
2013-07-15 12:57:13 -0400

    start to add real views with data

--- site/views.py
+++ site/views.py
@@ -1,16 +1,35 @@
-from flask import render_template
+from flask import render_template, abort
 from portfolio import app
+import route_manager
 
+manager = route_manager.RouteManager()
 
 # route declarations
 # load last 4 (or whatever) posts as child templates into home page
 @app.route('/')
 def load_index():
-    return render_template('index.html')
+    pages = manager.get_n_pages(4)
+    # need to add 'no pages found'
+    data = pages or {'test': { 'timestamp': 'today' }}
+    return render_template('index.html', data=data)
 
-# other routes here include /snippet, /demo, /article /<id> all child
-# templates that go into a container.html type page
+@app.route('/projects/')
+def load_projects():
+    return 'hello projects'
 
-# other main routes could be /about and /all (all posts)
+@app.route('/about/')
+def load_about():
+    return 'hello about'
 
+@app.route('/project/<id>')
+def load_page(id):
+    template = manager.get_page_template(id)
+    if template:
+        return render_template(template)
+    else:
+        abort(404)
 
+# error pages etc
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
