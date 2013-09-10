da6be6bb97fafed86704cdb00dbfb063bf840735
mbecks <beckwith.goldberg@gmail.com>
2013-09-10 12:17:32 -0400

    better error handling

--- portfolio/services.py
+++ portfolio/services.py
@@ -6,18 +6,24 @@ response_headers = {
     'Content-Type': 'application/json'
 }
 
+# TODO consider storing error messages for various http codes in a dict
+
 # so far ids = science2013
 # TODO: write service that fetches all ids
 @services.route('/scraper/<id>', methods=['GET'])
 def return_scraped_data(id):
-    try:
-        data = jsonify({ id: data })
-        response = make_response(data, 200)
-        for header in response_headers:
-            response.headers[header] = response_headers[header]
-    except:
     scraper = Scraper()
     data = scraper.scraper_get_scrape(id)
+    if data:
+        try:
+            data = jsonify({ id: data })
+            response = make_response(data, 200)
+            for header in response_headers:
+                response.headers[header] = response_headers[header]
+        except:
+            message = { 'Error': 'Internal Server Error, data malformed' }
+            response = make_response(jsonify(message), 500)
+    else:
         message = { 'Error': 'Scraper ID not found' }
         response = make_response(jsonify(message), 404)
     return response
