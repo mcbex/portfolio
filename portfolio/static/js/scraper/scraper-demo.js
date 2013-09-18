766ab9cb8450da0269177fed3689ea1905b8d9a3
mbecks <beckwith.goldberg@gmail.com>
2013-09-18 19:51:25 -0400

    starting to add visualizations, accept configuration object

--- portfolio/static/js/scraper/scraper-demo.js
+++ portfolio/static/js/scraper/scraper-demo.js
@@ -1,23 +1,60 @@
 (function() {
 
-    // do ajax call in demo, then create a visualizer instance for each data set?
-    // visualizer should accept data not make service calls
+    // do ajax call in demo, then create a visualizer instance for each data set
+    function fetchData(id) {
+        var url = 'http://' + document.location.host + '/services/scraper/' + id;
 
-    var visualizers = [];
+        return $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'json'
+        });
+    };
+
+    function makeDefaultChart(data) {
+        return new MarkupVisualizer({
+            title: 'Default',
+            data: data.science2013,
+            elem: '#demo-container',
+            dataProps: {
+                name: 'topic',
+                value: 'tags.length'
+            }
+        });
+    };
+
+    function makePerTermCharts(data) {
+        var visualizers = _.map(data.science2013, function(d) {
+            return new MarkupVisualizer({
+                title: d.topic,
+                data: d.tags,
+                elem: '#demo-container',
+                chartType: 'horizontalBar',
+                dataProps: {
+                    name: 'name',
+                    value: 'count'
+                }
+            });
+        });
+
+        return visualizers;
+    };
 
     window.onload = function(e) {
         var visualizer;
 
-        $.ajax({
-            url: 'http://' + document.location.host + '/services/scraper/science2013';
-            type: 'GET',
-            dataType: 'json',
-            success: function(resp) {
-                debugger;
-            },
-            error: function(resp) {
-                console && console.log(resp);
+        fetchData('science2013').then(function(resp) {
+            visualizer = makePerTermCharts(resp);
+
+            if (!visualizer.length) {
+                visualizer.render();
+            } else {
+                _.each(visualizer, function(v) {
+                    v.render();
+                });
             }
+        }, function(resp) {
+            console && console.log(resp);
         });
     };
 
--- portfolio/static/js/scraper/scraper.js
+++ portfolio/static/js/scraper/scraper.js
@@ -1,18 +1,134 @@
 /* data vis module for viewing trends in scraped markup
-* requires jQuery/Zepto, D3
-* updated 8/19/2013
+* requires D3
 */
 
-function MarkupVisualizer(data) {
-    // different graph types?
-    this.data = data;
+function MarkupVisualizer(config) {
+    var self = this;
+
+    _.each(config, function(val, key) {
+        self[key] = val;
+    });
+
+    this.init();
 };
 
-MarkupVisualizer.prototype.logger = function(data) {
-    console && console.log(data);
+MarkupVisualizer.prototype.logger = function(msg) {
+    return console && console.log(msg);
 };
 
-MarkupVisualizer.prototype.render = function(elem) {
-    // pass a dom elem for the graph to be rendered into
+// init is noop by default
+MarkupVisualizer.prototype.init = function() {};
+
+MarkupVisualizer.prototype.render = function() {
+    // if no chart type is specified use the default
+
+    if (this.chartType) {
+        this[this.chartType]();
+    } else {
+        this.defaultChart();
+    }
+};
+
+MarkupVisualizer.prototype._parseProp = function(data, prop) {
+    var test = /\./.test(prop),
+        props;
+
+    if (test) {
+        props = prop.split('.');
+
+        for (var i = 0; i < props.length; i++) {
+            if (data[props[i]]) {
+                data = data[props[i]];
+            } else {
+                data = undefined;
+            }
+        }
+    } else {
+        data = data[prop];
+    }
+
+    return data;
+};
+
+MarkupVisualizer.prototype.appendTitle = function() {
+    this.selection.append('div')
+        .attr('class', 'chart-title')
+        .text(this.title);
+};
+
+MarkupVisualizer.prototype.horizontalBar = function() {
+    var self = this, height = 320,
+        containerWidth, barWidth, scale,
+        textx, texty;
+
+    containerWidth = parseInt(d3.select(this.elem).style('width'));
+    barWidth = Math.floor(containerWidth / this.data.length);
+
+    scale = d3.scale.linear()
+        .range([5, height - 50])
+        .domain([0, d3.max(this.data, function(d) {
+            return +self._parseProp(d, self.dataProps.value);
+        })]);
+
+    this.selection = d3.select(this.elem).append('svg')
+        .attr('class', 'horizontalbar-chart')
+        .attr('width', '100%')
+        .attr('height', height)
+        .append('g')
+
+    this.selection.selectAll('.bar')
+        .data(this.data).enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('width', barWidth + 'px')
+        .attr('height', function(d, i) {
+            return scale(self._parseProp(d, self.dataProps.value));
+        })
+        .attr('x', function(d, i) {
+            return i * barWidth;
+        })
+        .attr('y', function(d) {
+            return height - scale(self._parseProp(d, self.dataProps.value));
+        });
+
+    this.selection.selectAll('text')
+        .data(this.data).enter()
+        .append('text')
+        .attr('x', function(d, i) {
+            return i * barWidth;
+        })
+        .attr('y', function(d) {
+            return height - scale(self._parseProp(d, self.dataProps.value));
+        })
+        .attr('transform', function(d, i) { 
+            textx = i * barWidth;
+            texty = height - scale(self._parseProp(d, self.dataProps.value));
+            return 'rotate(-90,' + textx + ',' + texty + ')'
+        })
+        .attr('dx', 5)
+        .attr('dy', (barWidth / 2) + 1)
+        .text(function(d) {
+            return self._parseProp(d, self.dataProps.name);
+        });
+
+    this.appendTitle();
+};
+
+MarkupVisualizer.prototype.defaultChart = function() {
+    var self = this;
+
+    this.selection = d3.select(this.elem).append('div')
+        .attr('class', 'default-chart');
+
+    this.selection.selectAll('div')
+        .data(this.data).enter()
+        .append('p').style('width', function(d) {
+            return (self._parseProp(d, self.dataProps.value) * 10) + 'px';
+        }).text(function(d) {
+            return self._parseProp(d, self.dataProps.name)
+                + ' (' + self._parseProp(d, self.dataProps.value) + ')';
+        });
+
+    this.appendTitle();
 };
 
