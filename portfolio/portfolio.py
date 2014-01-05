c0728c8952c3eec51fbbdd6cfc4cc657f0258ebd
mcbex <ming.beckwith@gmail.com>
2014-01-05 16:46:33 -0500

    add jinja extension so horrible whitespace goes away

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -2,8 +2,10 @@ from flask import Flask
 from services import services
 from views import views
 from errorviews import errorviews
+from extensions.jinja2htmlcompress import HTMLCompress
 
 app = Flask('portfolio')
+app.jinja_env.add_extension(HTMLCompress)
 app.register_blueprint(services, url_prefix='/services')
 app.register_blueprint(views)
 app.register_blueprint(errorviews)
