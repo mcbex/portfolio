b92aff57b368900300429cd47078ebf515398977
mcbex <ming.beckwith@gmail.com>
2014-05-16 10:56:21 -0400

    add route for articles

--- portfolio/views.py
+++ portfolio/views.py
@@ -8,9 +8,13 @@ manager = RouteManager()
 # load last 4 (or whatever) posts as child templates into home page
 @views.route('/')
 def load_index():
-    projects = manager.get_n_projects(4)
+    projects = manager.get_n_projects('project', 4)
+    articles = manager.get_n_projects('article', 2)
     # need to add 'no pages found'
-    data = projects or {'test': { 'timestamp': 'today' }}
+    data = {
+        'projects': projects,
+        'articles': articles
+    }
     return render_template('index.html', data=data)
 
 @views.route('/projects/')
@@ -19,11 +23,15 @@ def load_projects():
     return render_template('projects.html', data=projects)
 
 @views.route('/articles/')
-def load_about():
-    return 'hello about'
+def load_articles():
+    articles = manager.get_n_projects('article')
+    return render_template('articles.html', data=articles)
 
+# TODO come up with clear definitions for projects vs articles or other
+# entities - clean up data and either make everything one type or
+# abstract things in a more reasonable way
 @views.route('/project/<id>')
-def load_page(id):
+def load_project(id):
     project = manager.get_project_byid(id)
     template = project['template']
     if template:
@@ -31,3 +39,11 @@ def load_page(id):
     else:
         abort(404)
 
+@views.route('/article/<id>')
+def load_article(id):
+    article = manager.get_project_byid(id)
+    template = article['template']
+    if template:
+        return render_template(template, data=article)
+    else:
+        abort(404)
